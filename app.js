db4b7b0fea1f9f344466004ed2a7a6d350b31565
Daniel Rares <danielrares1@yahoo.com>
2020-11-12 17:29:46 -0300

    criando os tratamentos para erros

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
-const express = require('express');
+const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
-const rotaProdutos = require('./routes/produtos'); 
-const rotaPedidos = require('./routes/pedidos');
+const rotaProdutos = require("./routes/produtos");
+const rotaPedidos = require("./routes/pedidos");
 
-app.use('/produtos', rotaProdutos);
-app.use('/pedidos', rotaPedidos);
+app.use(morgan("dev"));
 
-app.use('/teste', (req, res, next) => {
-    res.status(200).send({
-        mensagem: 'preparando esqueleto'
-    })
+app.use("/produtos", rotaProdutos);
+app.use("/pedidos", rotaPedidos);
+
+app.use((req, res, next) => {
+  const erro = new Error("not found");
+  erro.status = 404;
+  next(erro);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  return res.send({
+    erro: {
+      mensagem: error.message
+    }
+  });
 });
 
 module.exports = app;
